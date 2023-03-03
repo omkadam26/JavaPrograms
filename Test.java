af260e3dcab634b4f00bfd14e1b4ea862c08b136
omkadam26 <97826707+omkadam26@users.noreply.github.com>
2023-03-03 21:15:47 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Start @omkadam");
+        
+        Parent p=new Parent();
+
+
+         System.out.println("ENd @omkadam");
+    }
+    
+
+}
